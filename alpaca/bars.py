aa59db435ce951bd07205be343ee4287e753efb1
Harry Luk <74358636+HarryLukKF@users.noreply.github.com>
2022-10-15 15:38:22 +0800

    alpaca

--- alpaca/bars.py
+++ alpaca/bars.py
@@ -1,13 +1,13 @@
 from numpy import rot90
 import config, requests, json, pandas as pd
-
+from datetime import datetime, timedelta
 
 def getdata(coin0, coin1):
         
     #minute_bar_url = config.BARS_URL + '/5Min?symbols=BTC/USD&limit=1000'
     # minute_bar_url = config.BARS_URL + '?timeframe=5Min&symbols=BTC/USD,ETH/USD&limit=20'
-    minute_bar_url_coin0 = config.BARS_URL + f'?timeframe=5T&symbols={coin0}&limit=1000'
-    minute_bar_url_coin1 = config.BARS_URL + f'?timeframe=5T&symbols={coin1}&limit=1000'
+    minute_bar_url_coin0 = config.BARS_URL + f'?timeframe=5T&symbols={coin0}&limit=1000&start={(datetime.today() - timedelta(1)).strftime("%Y-%m-%d")}'
+    minute_bar_url_coin1 = config.BARS_URL + f'?timeframe=5T&symbols={coin1}&limit=1000&start={(datetime.today() - timedelta(1)).strftime("%Y-%m-%d")}'
 
     r0 = requests.get(minute_bar_url_coin0, headers=config.HEADERS)
     r1 = requests.get(minute_bar_url_coin1, headers=config.HEADERS)
@@ -15,6 +15,6 @@ def getdata(coin0, coin1):
     data0 = pd.DataFrame(r0.json()['bars'][coin0])[['t','o','h','l','c','v']].rename(columns={'t':'datetime', 'o':'open', 'h':'high', 'l':'low', 'c':'close', 'v':'volume'})
     data1 = pd.DataFrame(r1.json()['bars'][coin1])[['t','o','h','l','c','v']].rename(columns={'t':'datetime', 'o':'open', 'h':'high', 'l':'low', 'c':'close', 'v':'volume'})
 
-    return data0[-20:], data1[-20:]
+    return data0[-20:].reset_index(), data1[-20:].reset_index()
 
 
--- alpaca/trade.py
+++ alpaca/trade.py
@@ -1,17 +1,32 @@
-import requests, json
+import requests, json, schedule, time
 from config import *
 from bars import getdata
+from datetime import datetime
 
 BASE_URL = "https://paper-api.alpaca.markets"
 ACCOUNT_URL = "{}/v2/account".format(BASE_URL)
 ORDERS_URL = "{}/v2/orders".format(BASE_URL)
 POSITIONS_URL = "{}/v2/positions".format(BASE_URL)
+QUOTE_URL = "{}/v1beta2/crypto/latest/trades".format(BASE_URL)
 HEADERS = {'APCA-API-KEY-ID': API_KEY, 'APCA-API-SECRET-KEY': SECRET_KEY}
+
+enter_std=0.5
+exit_std=0.5  # 0.5
+stop_loss=0.95  # 0.9
+long_BCH = False
+long_BTC = False
 
 def get_account():
     r = requests.get(ACCOUNT_URL, headers=HEADERS)
     return json.loads(r.content)
 
+def get_cash():
+    r = requests.get(ACCOUNT_URL, headers=HEADERS)
+    return float(json.loads(r.content)['non_marginable_buying_power'])
+
+def get_portfolio():
+    r = requests.get(ACCOUNT_URL, headers=HEADERS)
+    return float(json.loads(r.content)['portfolio_value'])
 
 def create_order(symbol, qty, side, type, time_in_force):
     data = {
@@ -40,34 +55,48 @@ def close_all_position():
     r = requests.delete(POSITIONS_URL, headers=HEADERS)
     return json.loads(r.content)
 
+def get_quote(symbol):
 
+    r = requests.get(f"https://data.alpaca.markets/v1beta2/crypto/latest/quotes?symbols={symbol}", headers=HEADERS)
 
+    return json.loads(r.content)['quotes'][symbol]['ap']
 
-enter_std=0.5
-exit_std=0.5  # 0.5
-stop_loss=-0.01  # -0.015
+def trade():
+    ### Collect data and calculate signal
+    data0, data1 = getdata('BTC/USD', 'BCH/USD')
+    mean = (data0['close'] - data1['close']).mean()
+    std = (data0['close'] - data1['close']).std()
+    enter_upper = mean + (enter_std * std)
+    exit_upper = mean + (exit_std * std)
+    enter_lower = mean - (enter_std * std)
+    exit_lower = mean - (exit_std * std)
+    spread = float(data0[-1:]['close']) - float(data1[-1:]['close'])
+    print(f'Current time : {datetime.now()}\nSpread : {spread}\nUpper and lower : {enter_upper, enter_lower}')
+    if get_position() == []:
+        if spread > enter_upper:
+            create_order('BCH/USD', side = 'buy', qty = get_cash()/get_quote('BCH/USD')-1, type = 'market', time_in_force = 'gtc')
+            int_portfolio = get_portfolio()
+            long_BCH = True
+        elif spread < enter_lower:
+            create_order('BTC/USD', side = 'buy', qty = get_cash()/get_quote('BTC/USD')-0.1,type = 'market', time_in_force = 'gtc')
+            int_portfolio = get_portfolio()
+            long_BTC = True
+        print(get_position(), get_portfolio)
+    ###check exist###
+    if get_position() != []:
+        if get_portfolio()/int_portfolio <= stop_loss:
+            close_all_position()
+            long_BTC = False
+            long_BCH = False
+        if long_BCH and spread < exit_upper:
+            close_all_position()
+            long_BCH = False
+        if long_BTC and spread > exit_lower:
+            close_all_position()
+            long_BTC = False
 
-data0, data1 = getdata('BTC/USD', 'ETH/USD')
-
-
-mean = (data0['close'] - data1['close']).mean()
-std = (data0['close'] - data1['close']).std()
-
-
-enter_upper = mean + (enter_std * std)
-exit_upper = mean + (exit_std * std)
-enter_lower = mean - (enter_std * std)
-exit_lower = mean - (exit_std * std)
-
-spread = data0[-1:]['close'] - data1[-1:]['close']
-
-# if spread > enter_upper:
-#     create_order('BTCUSD', side = 'sell', type = 'market', time_in_force = 'gtc')
-#     create_order('ETHUSD', side = 'buy', type = 'market', time_in_force = 'gtc')
-# elif spread < enter_lower:
-#     create_order('BTCUSD', side = 'buy', type = 'market', time_in_force = 'gtc')
-#     create_order('ETHUSD', side = 'sell', type = 'market', time_in_force = 'gtc')
-
-
-
-print(data0, data1)
+schedule.every(5).minutes.do(trade)
+  
+while True:
+    schedule.run_pending()
+    time.sleep(1)
